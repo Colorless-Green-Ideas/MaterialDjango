b91f03768da47405319ecb90e1ac57e9e442e2c2
jrabbit <jackjrabbit+github@gmail.com>
2015-03-30 02:55:36 -0400

    add passwordinput

--- materialdjango/forms.py
+++ materialdjango/forms.py
@@ -1,9 +1,14 @@
-from widgets import PaperTextInput
+from django import forms
+from widgets import PaperTextInput, PaperPasswordInput
+
 
 def mangle_form(form):
     "Utility to monkeypatch forms into paperinputs, untested"
     for field in form.fields:
         if type(field.widget) is forms.widgets.Textarea:
             field.widget = PaperTextInput()
+            field.label = ''
+        if type(field.widget) is forms.widgets.PasswordInput:
+            field.widget = PaperPasswordInput()
             field.label = ''
     return form
--- materialdjango/widgets.py
+++ materialdjango/widgets.py
@@ -1,4 +1,4 @@
-from django.forms.widgets import TextInput
+from django.forms.widgets import TextInput, PasswordInput
 from django.utils.html import format_html
 
 
@@ -14,5 +14,19 @@ class PaperTextInput(TextInput):
         else:
             html = """<paper-input-decorator label='{0}' floatingLabel>
             <input is="core-input" name="{0}" value="{1}">
+            </paper-input-decorator>"""
+            return format_html(html, name, value)
+
+class PaperPasswordInput(PasswordInput):
+    def render(self, name, value, attrs=None):
+        if value is None:
+            html = """<paper-input-decorator label='{0}' floatingLabel>
+            <input is="core-input" name="password" type="password"/>
+            </paper-input-decorator>"""
+            print html
+            return format_html(html, name)
+        else:
+            html = """<paper-input-decorator label='{0}'  type="password" floatingLabel>
+            <input is="core-input" name="password" type="password" value="{1}"/>
             </paper-input-decorator>"""
             return format_html(html, name, value)
