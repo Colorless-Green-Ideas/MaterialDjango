27ad1c0243bff001cf12406ce65feadb94de4270
jrabbit <jackjrabbit+github@gmail.com>
2015-04-02 01:21:05 -0400

    template tag

---
+++ materialdjango/templatetags/polymerdep.py
@@ -0,0 +1,15 @@
+from django import template
+from django.conf import settings
+from django.core.urlresolvers import reverse_lazy
+from django.templatetags.static import static
+
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def dep(value="polymer/polymer.html"):
+    # should you really be able to do this over reverse_lazy???
+    static_url = static(
+        "materialdjango/components/bower_components/%s" % value)
+    return '<link rel="import" href="{0}">'.format(static_url)
