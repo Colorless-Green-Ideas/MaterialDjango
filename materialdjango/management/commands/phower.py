320cb84361b2c1652836e9eba0902295f88fcf67
jrabbit <jackjrabbit+github@gmail.com>
2015-04-02 14:16:00 -0400

    does this fix?

--- materialdjango/management/commands/phower.py
+++ materialdjango/management/commands/phower.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
-    args =
+    # args =
     help = "Replaces Bower for installing webcompnents"
--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 #! /usr/bin/env python
 
 from setuptools import setup
+from setuptools import find_packages
+
+
 setup(name="MaterialDjango",
-      version="0.1a1",
-      packages=["materialdjango"],
+      version="0.1a2",
+      packages=find_packages(),
       # scripts=[],
       author="Jack Laxson",
       author_email="jack@getpizza.cat",
       description="",
-      #license
+      license="MIT License",
+      include_package_data=True,
       install_requires=["django", "vulcanize", 'bower.py'],
       url="https://github.com/Colorless-Green-Ideas/MaterialDjango",
       classifiers=["License :: OSI Approved :: MIT License", 
-      "Framework :: Django", "Topic :: Text Processing :: Markup :: HTML",]
+      "Framework :: Django", "Topic :: Text Processing :: Markup :: HTML", 
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',]
       )
