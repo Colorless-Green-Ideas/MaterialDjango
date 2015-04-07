661594ec7eeb8c597131080ebeee2c6323049eb5
jrabbit <jackjrabbit+github@gmail.com>
2015-04-07 02:19:34 -0400

    error in mangler (test in tinylibrary)

--- materialdjango/forms.py
+++ materialdjango/forms.py
@@ -5,7 +5,7 @@ from widgets import PaperTextInput, PaperPasswordInput
 def mangle_form(form):
     "Utility to monkeypatch forms into paperinputs, untested"
     for field in form.fields:
-        if type(field.widget) is forms.widgets.Textarea:
+        if type(field.widget) is forms.widgets.TextInput:
             field.widget = PaperTextInput()
             field.label = ''
         if type(field.widget) is forms.widgets.PasswordInput:
