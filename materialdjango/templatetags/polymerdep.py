d6e515dbf680cf65c8966db1ca1e4104f9a90bf7
jrabbit <jackjrabbit+github@gmail.com>
2015-04-12 16:15:25 -0400

    not functional still

--- materialdjango/templatetags/polymerdep.py
+++ materialdjango/templatetags/polymerdep.py
@@ -13,5 +13,8 @@ def dep(value="polymer/polymer.html"):
         "materialdjango/components/bower_components/%s" % value)
     return '<link rel="import" href="{0}">'.format(static_url)
 
-def shim():
-    return  "<script src='{% static 'materialdjango/components/bower_components/webcomponentsjs/webcomponents.js' %}'></script>"
+
+@register.simple_tag
+def polymer_shim():
+    static_url = static('materialdjango/components/bower_components/webcomponentsjs/webcomponents.js')
+    return "<script src='{0}'></script>".format(static_url)
