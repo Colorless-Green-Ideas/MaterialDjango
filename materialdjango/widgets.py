40252b9de066919471add5e2155c694ac16286de
jrabbit <jackjrabbit+github@gmail.com>
2015-04-18 16:03:42 -0400

    should probably figure out how to subclass this stuff but it works

--- materialdjango/widgets.py
+++ materialdjango/widgets.py
@@ -1,6 +1,7 @@
-from django.forms.widgets import TextInput, PasswordInput
+from django.forms.widgets import TextInput, PasswordInput, EmailInput
 from django.utils.html import format_html
 
+# ref https://github.com/django/django/blob/stable/1.8.x/django/forms/widgets.py
 
 class PaperTextInput(TextInput):
 
@@ -32,5 +33,21 @@ class PaperPasswordInput(PasswordInput):
         else:
             html = """<paper-input-decorator label='{0}'  type="password" floatingLabel>
             <input is="core-input" name="password" type="password" value="{1}"/>
+            </paper-input-decorator>"""
+            return format_html(html, name, value)
+
+class PaperEmailInput(EmailInput):
+    def render(self, name, value, attrs=None):
+    # Unlike inputs using paper-input-decorator directly,
+    # paper-input does not work out of the box with the native form
+    # element.
+        if value is None:
+            html = """<paper-input-decorator label='{0}' floatingLabel autoValidate>
+            <input is="core-input" name="{0}" type="email">
+            </paper-input-decorator>"""
+            return format_html(html, name)
+        else:
+            html = """<paper-input-decorator label='{0}' floatingLabel autoValidate>
+            <input is="core-input" name="{0}" value="{1}" type="email">
             </paper-input-decorator>"""
             return format_html(html, name, value)
