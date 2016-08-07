73a754b23b9e9a5a8e49020f2179b822e714fc94
jrabbit <jackjrabbit@gmail.com>
2016-08-07 19:36:45 -0400

    version bump for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 
 setup(name="MaterialDjango",
-      version="0.2.3",
+      version="0.2.4",
       packages=find_packages(),
       # scripts=[],
       author="Jack Laxson",
